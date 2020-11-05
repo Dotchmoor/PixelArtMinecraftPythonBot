0ab6242e00003f278f58c05672aeca38840c5785
Louis Schirmer <louis@schirmer.org>
2020-11-05 21:17:07 +0100

    Milestone 2 - better colors and direct image creation

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ json_blockcolors = twod_block_module.block_manager(datapath)
 img = cv2.imread(os.path.join(imgpath, random.choice(os.listdir(imgpath))))
 twod_pixel_map = twod_block_module.pixel_img(img, json_blockcolors.get_data())
 com_list = twod_pixel_map.get_command_list()
+twod_pixel_map.create_img(datapath, 16)
 
-print("click in minecraft text")
-time.sleep(5)
-for entry in com_list:
-    if keyboard.is_pressed('q') == False:
-        keyboard.write(entry)
-        keyboard.press_and_release('enter')
-        keyboard.press_and_release('t')
-        time.sleep(0.085)
+#print("click in minecraft text")
+#time.sleep(5)
+#for entry in com_list:
+#    if keyboard.is_pressed('q') == False:
+#        keyboard.write(entry)
+#        keyboard.press_and_release('enter')
+#        keyboard.press_and_release('t')
+#        time.sleep(0.085)
 
-keyboard.press_and_release('esc')
+#keyboard.press_and_release('esc')
--- twod_block_module.py
+++ twod_block_module.py
@@ -1,4 +1,4 @@
-import json, os, random, cv2
+import json, os, random, cv2, numpy
 
 class block_manager():
     def __init__(self, folder_with_blocks):
@@ -8,8 +8,8 @@ class block_manager():
         #data
         if "block_color_values.json" not in os.listdir(self.block_path):
             jsondata = {}
-            with open(os.path.join(self.path, "block_color_values.json"), 'w') as jsonfile:
-                blockpath = os.path.join(self.path, "blocks")
+            with open(os.path.join(self.block_path, "block_color_values.json"), 'w') as jsonfile:
+                blockpath = os.path.join(self.block_path, "blocks")
                 for block in os.listdir(blockpath):
                     average = {}
                     activeblock = cv2.imread(os.path.join(blockpath, block))
@@ -41,6 +41,7 @@ class block_manager():
 
 class pixel_img:
     def __init__(self, img_list, block_color_values):
+        self.img_list = img_list
         self.blocks_to_img = self.__get_best_suiting_block(block_color_values, img_list)
         print(self.blocks_to_img)
         self.command_list = self.__generate_commands(self.blocks_to_img)
@@ -48,6 +49,30 @@ class pixel_img:
     def get_command_list(self):
         return self.command_list
     
+    def create_img(self, data_path, block_pxwh):
+        img_list = [[] for line in range(len(self.img_list)*block_pxwh)]
+        
+        startline = 0
+
+        for block_line in self.blocks_to_img:
+            if len(img_list[startline]) >= len((self.img_list[0])*block_pxwh):
+                startline += block_pxwh
+            for block in block_line:
+                for element in os.listdir(os.path.join(data_path, "blocks")) :
+                    if block in element:
+                        y = 0
+                        for line in cv2.imread(os.path.join(data_path, "blocks", element)):
+                            for pixel in line:
+                                try:
+                                    print(len(img_list),startline + y)
+                                    img_list[startline + y].append(pixel)
+                                except IndexError:
+                                    cv2.imwrite(os.path.join(data_path, "last_run.png"), numpy.array(img_list))
+                                    return 1
+                            y += 1
+                        break
+        cv2.imwrite(os.path.join(data_path, "last_run.png"), numpy.array(img_list))            
+
     def __get_best_suiting_block(self, block_color_values, img_list):
         block_list = [[[None] for x in y] for y in img_list]
         
@@ -82,7 +107,8 @@ class pixel_img:
                             print(leftspots)
                             print("Key Error", line, pixel)
                             print(KeyError)
-            tollerance += 10
+            
+            tollerance += 5
             print(tollerance)
 
         return [[x[0] for x in y] for y in block_list]
